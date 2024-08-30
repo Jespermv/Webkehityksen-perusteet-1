bfaff3bfe30db2c8de156c0d101eb60d65181cbd
Jespermv <jesper.vaisanen@metropolia.fi>
2024-08-30 11:14:41 +0300

    dom

--- Tehtavat/ohjelmointi-tehtavat/dom/dom-assignments/dom-assignments/t2/t2.js
+++ Tehtavat/ohjelmointi-tehtavat/dom/dom-assignments/dom-assignments/t2/t2.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const todoList = [
     {id: 1, task: 'Learn HTML', completed: true},
     {id: 2, task: 'Learn CSS', completed: true},
-    {id: 3, task: 'Learn JS', completed: true},
+    {id: 3, task: 'Learn JS', completed: false},
     {id: 4, task: 'Learn TypeScript', completed: false},
     {id: 5, task: 'Learn React', completed: false},
   ];
